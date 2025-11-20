a770a7920b78464b4c292e9fb4253c6673716b3f
KAAli-DBS <kanzaali.manzar@dbs.ie>
2025-11-20 12:04:26 +0000

    Threading

---
+++ ParallelThreads/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        Thread thread = new Thread(PrintNumbers);
+        thread.Start();
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"Main thread: {i}");
+            Thread.Sleep(500); 
+        }
+        thread.Join();
+        Console.WriteLine("Both threads are complete.");
+    }
+
+    static void PrintNumbers()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"Worker thread: {i}");
+            Thread.Sleep(500); 
+        }
+    }
+}
