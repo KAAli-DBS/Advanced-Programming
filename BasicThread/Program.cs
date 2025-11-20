22e6a0160e3bc702e9b0eaf8baa1486d95b85cf3
KAAli-DBS <kanzaali.manzar@dbs.ie>
2025-11-20 12:04:00 +0000

    Threading

---
+++ BasicThread/Program.cs
@@ -0,0 +1,23 @@
+﻿// To find-out about how many cores and logical cores your GitHub has: 
+
+// Console.WriteLine("Cores count: " + Environment.ProcessorCount);
+
+using System;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        // Create and start a thread
+        Thread thread = new Thread(() => Console.WriteLine("Hello from the new thread!"));
+        thread.Start();
+
+        // Main thread continues
+        Console.WriteLine("Hello from the main thread!");
+
+        // Wait for the thread to finish
+        thread.Join();
+    }
+}
+
