165dd3b3feeed8891f102783490a7583e8a686ff
KAAli-DBS <kanzaali.manzar@dbs.ie>
2025-11-27 15:39:35 +0000

    Network Programming

---
+++ DataHTTP/Program.cs
@@ -0,0 +1,17 @@
+﻿
+// Execute this code example in your Visual Studio or Visual Studio Code (VS Code) 
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+class Program
+{
+    static async Task Main()
+    {
+        HttpClient client = new HttpClient();
+        string url = "https://worldtimeapi.org/api/timezone/Etc/UTC";
+
+        string response = await client.GetStringAsync(url);
+        Console.WriteLine("Response from server:");
+        Console.WriteLine(response);
+    }
+}
---
+++ PingUtility/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter hostname or IP to ping: ");
+        string host = Console.ReadLine();
+
+        Ping ping = new Ping();
+        PingReply reply = ping.Send(host);
+
+        if (reply.Status == IPStatus.Success)
+        {
+            Console.WriteLine($"Ping successful. Roundtrip time: {reply.RoundtripTime} ms");
+        }
+        else
+        {
+            Console.WriteLine("Ping failed.");
+        }
+    }
+}
